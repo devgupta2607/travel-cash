b35b095292ff3aaf47698dd679953bf0092fb82c
Dev Gupta <devgupta2607@gmail.com>
2019-09-19 18:50:56 +0530

    Added screenshots

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
 # travel_cash
 
-A new Flutter project.
+An application to provide you travel cash for using public transportation. And using this travel cash, you can avail various offers and coupons for the shops around the destination of your travel.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## Screenshots
+<img src="./screenshots/1.jpeg" alt="drawing" width="100"/>
+<img src="./screenshots/5.jpeg" alt="drawing" width="100"/>
+<img src="./screenshots/7.jpeg" alt="drawing" width="100"/>
+<img src="./screenshots/9.jpeg" alt="drawing" width="100"/>
--- lib/slides/cash.dart
+++ lib/slides/cash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../states/app_state.dart';
 import 'package:provider/provider.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Cash extends StatelessWidget {
   @override
@@ -33,6 +34,24 @@ class _DisplaycashState extends State<Displaycash> {
             SizedBox(height: 10.0,),
             Text('${appState.distance ~/ 1000} Travel Cash Earned 🎉',
             style: TextStyle(fontSize: 20.0),),
+            SizedBox(height: 10.0,),
+            ButtonTheme(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+            
+                Text('Coins'),
+                Image.asset(
+                  'images/tc_logo.png',
+                  width: 40.0,
+                  height: 40.0,
+                ),
+                SizedBox(width: 3.0,),
+                Text('x2'),
+                SizedBox(width: 3.0,),
+                Icon(FontAwesomeIcons.film),
+              ],)
+            )
           ],
         ),
         ),
